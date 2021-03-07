d000c5f79bd5b731c8ab8e79c057817dfcff8ed7
Alejandro <aleplacambra@gmail.com>
2021-03-07 18:15:39 +0100

    Column logic

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,9 +1,24 @@
 import React from "react";
-
-export default class Column {
+import Tile from "../Tile/Tile";
+import styles from "./Column.module.css";
+import { Props } from "./types";
+export default class Column extends React.PureComponent<Props> {
   render() {
-    return (
-        <div></div>
-    );
+    const { column, rows, chipsPositions, onTileClick } = this.props;
+    const tiles = [];
+    
+    for (let row = 0; row < rows; row++) {
+      const tileId = `${row}:${column}`;
+      const chipType = chipsPositions[tileId];
+      tiles.push(
+        <Tile
+          key={tileId}
+          id={tileId}
+          chipType={chipType}
+          onClick={onTileClick}
+        />
+      );
+    }
+    return <div className={styles.column}>{tiles}</div>;
   }
 }

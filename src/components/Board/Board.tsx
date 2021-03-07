2d0271ac4b65180eb2cae1aee8ecb03e4959b631
Alejandro <aleplacambra@gmail.com>
2021-03-07 18:39:45 +0100

    Board logic

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,10 +1,26 @@
 import React from "react";
-
-export default class Board {
-
+import Column from "../Column/Column";
+import styles from "./Board.module.css";
+import { Props } from "./types";
+export default class Board extends React.PureComponent<Props> {
+  
+  renderColumns() {
+    const { columns, rows, chipsPositions, onTileClick } = this.props;
+    const columnsComponents = [];
+    for (let column = 0; column < columns; column++) {
+      columnsComponents.push(
+        <Column
+          key={column}
+          column={column}
+          rows={rows}
+          chipsPositions={chipsPositions}
+          onTileClick={onTileClick}
+        />
+      );
+    }
+    return <>{columnsComponents}</>;
+  }
   render() {
-    return (
-        <div></div>
-    );
+    return <div className={styles.board}>{this.renderColumns()}</div>;
   }
 }

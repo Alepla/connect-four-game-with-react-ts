2eb7f2678d0e293f3e0a4b51e1f1e02352d9d2ff
Alejandro <aleplacambra@gmail.com>
2021-03-07 18:14:28 +0100

    Tile logic

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-
-export default class Tile {
+import classNames from "classnames";
+import styles from "./Tile.module.css";
+import { Props } from "./types";
+export default class Tile extends React.PureComponent<Props> {
   render() {
+    const { id, chipType, onClick = () => {} } = this.props;
+    const chipCssClass = classNames(styles.chip, chipType === "red" ? styles.red : styles.yellow);
     
     return (
-      <div>
+      <div className={styles.tile} onClick={() => onClick(id)}>
+        {chipType && <div className={chipCssClass} />}
       </div>
     );
   }
